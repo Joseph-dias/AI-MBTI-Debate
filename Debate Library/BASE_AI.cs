bc615e167f14d197829bbbe494cecba1431fe5e9
SoftwareNugget <j.dean.dias@gmail.com>
2026-02-19 20:27:35 -0700

    Add project files.

---
+++ Debate Library/BASE_AI.cs
@@ -0,0 +1,36 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+namespace Debate_Library
+{
+    public abstract class BASE_AI
+    {
+        private string? apiKey;
+        protected OpenAIClient client;
+        protected string model;
+
+        //Chat message list
+        protected abstract List<ChatMessage> messages { get; set; }
+        public BASE_AI(string model) 
+        {
+            apiKey = Environment.GetEnvironmentVariable("XAI_API_KEY") ?? throw new InvalidOperationException("Set XAI_API_KEY environment variable");
+            client = new OpenAIClient( //Creating the client
+            new ApiKeyCredential(apiKey),
+            new OpenAIClientOptions
+            {
+                Endpoint = new Uri("https://api.x.ai/v1")
+            });
+
+            this.model = model; //Set the model
+        }
+
+        protected async IAsyncEnumerable<StreamingChatCompletionUpdate> sendRequestAsync()
+        {
+            await foreach (StreamingChatCompletionUpdate update in client.GetChatClient(model).CompleteChatStreamingAsync(messages))
+            {
+                yield return update;
+            }
+        }
+    }
+}
